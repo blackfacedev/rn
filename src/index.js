3197bcaea323514927cfba1b6c116d9304e24cfc
Marcel Assis <marcel@marcelassis.com>
2019-08-08 19:19:19 -0300

    Remove todo component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 import './config/ReactotronConfig';
 import './config/DevToolsConfig';
 
-import Todo from './components/Todo';
-
 export default class App extends Component {
-
-  state = {
-    todos: [
-      { id: 0, text:'Fazer cafe'},
-      { id: 1, text: 'Estudar GoNative'},
-    ]
-  }
-
-  addTodo = () => {
-    this.setState({ todos: [...this.state.todos, {id: Math.random(), text:'Novo Todo'}]})
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        {this.state.todos.map( todo => <Todo title={todo.text} key={todo.id}/>)}
-        <Button title="Adicionar Todo" onPress={this.addTodo} />
+        <Text> Hello </Text>
       </View>
     );
   }
